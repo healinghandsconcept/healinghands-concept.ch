3bdc6988173c0aac30412d3fd527c2ba7b2f39d8
cedricv <cedric@cedricv.com>
2026-02-18 11:26:06 +0100

    Initial commit for Healing Hands Concept 2026 website

---
+++ eleventy.config.js
@@ -0,0 +1,48 @@
+const Image = require("@11ty/eleventy-img");
+
+module.exports = function (eleventyConfig) {
+  // Passthrough copy for assets
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  // Watch targets
+  eleventyConfig.addWatchTarget("src/assets/css/");
+  eleventyConfig.addWatchTarget("src/assets/js/");
+
+  // Image Shortcode
+  eleventyConfig.addShortcode("image", async function (src, alt, sizes = "100vw") {
+    if (alt === undefined) {
+      // You bet we throw an error on missing alt (alt="" works okay)
+      throw new Error(`Missing \`alt\` on myImage from: ${src}`);
+    }
+
+    let metadata = await Image(src, {
+      widths: [300, 600, 900, 1200],
+      formats: ["webp", "jpeg"],
+      urlPath: "/assets/img/optimized/",
+      outputDir: "./_site/assets/img/optimized/"
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    // You bet we throw an error on missing alt (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "_site",
+      includes: "_includes",
+      data: "_data"
+    },
+    templateFormats: ["md", "njk", "html"],
+    markdownTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dataTemplateEngine: "njk"
+  };
+};
